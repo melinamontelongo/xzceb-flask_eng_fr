0a10ee0b1e74b779d58652426d54a525464bab7e
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-01-25 19:36:14 -0300

    fixed tests path

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,19 +1,21 @@
 import unittest
-import sys
-sys.path.append("..")
-from machinetranslation.translator import english_to_french, french_to_english
+import os, sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french("Yes"), "Oui")
-        self.assertEqual(english_to_french("Thank you"), "Je vous remercie")
+        self.assertNotEqual(english_to_french("Thank you"), "Thank you")
         self.assertIsNone(english_to_french(None))        #Checking for null input
         self.assertIsNotNone(french_to_english("Cat"))    #Checking for not null input
         self.assertEqual(english_to_french("Hello"), "Bonjour")
 
     def test_french_to_english(self):
         self.assertEqual(french_to_english("Oui"), "Yes")
-        self.assertEqual(french_to_english("Merci"), "Thank you")
+        self.assertNotEqual(french_to_english("Merci"), "Merci")
         self.assertIsNone(french_to_english(None))        #Checking for null input
         self.assertIsNotNone(french_to_english("Chat"))   #Checking for not null input
         self.assertEqual(french_to_english("Bonjour"), "Hello")
